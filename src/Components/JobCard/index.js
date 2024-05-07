d2b271ca4974b0b274e3cb72b95c8435532171c8
arp99 <arpan19991025@gmail.com>
2024-05-07 19:05:20 +0530

    Add card styles, infinite scroll support

---
+++ src/Components/JobCard/index.js
@@ -0,0 +1,41 @@
+import Styles from "./index.module.css";
+
+export const JobCard = ({ job }) => {
+  return (
+    <div className={Styles["job-card__container"]}>
+      <div className={Styles["card-content"]}>
+        <div className={Styles["job-info"]}>
+          <div className={Styles["company-info__container"]}>
+            <div className={Styles["company-name-role__container"]}>
+              <h3 className={Styles["company-name"]}>{job.companyName}</h3>
+              <h2 className={Styles["job-position"]}>{job.jobRole}</h2>
+            </div>
+            <p className={Styles["job-location"]}>{job.location}</p>
+          </div>
+        </div>
+        <div className={Styles["job-salary-range"]}>
+          Estimated Salary:{" "}
+          {job.minJdSalary
+            ? `${job.salaryCurrencyCode}${job.minJdSalary} - ${job.maxJdSalary}`
+            : `${job.salaryCurrencyCode}${job.maxJdSalary}`}
+        </div>
+        <div className={Styles["company-info"]}>
+          <div>
+            <p className={Styles["heading"]}>About Company</p>
+            <div className={Styles["job-description"]}>
+              {job.jobDetailsFromCompany}
+            </div>
+          </div>
+        </div>
+        <div className={Styles["show-more-action"]}>Show More</div>
+        <div className={Styles["job-experience-info"]}>
+          <h3>Minimum Experience</h3>
+          <h2>{job?.minExp} years</h2>
+        </div>
+      </div>
+      <div className={Styles["action-buttons__container"]}>
+        <div className={Styles["apply-btn"]}>Easy Apply</div>
+      </div>
+    </div>
+  );
+};
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useCallback, useState } from "react";
 import { JobListingAPI } from "../api";
-import { useCallback } from "react";
-import { useState } from "react";
+import { JobCard } from "../Components";
+import Styles from "./index.module.css";
+import { useInView } from "react-intersection-observer";
 
 export const Home = () => {
   const [jobsData, setJobsData] = useState({
@@ -12,19 +13,45 @@ export const Home = () => {
       totalPages: 0, //calculated on basis of total data
     },
   });
+  const [isFetching, setIsFetching] = useState(false);
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    trackVisibility: true,
+    delay: 500,
+    threshold: 0,
+  });
+
   const getJobs = useCallback(
     async (pagination = {}) => {
       try {
         const { data } = await JobListingAPI.POST.getJobListing(pagination);
-        setJobsData((prev) => ({
-          ...prev,
-          data: data?.jdList,
-          pagination: {
-            total: data.totalCount,
-            offset: pagination.offset,
-            totalPages: Math.ceil(data.totalCount / pagination.limit),
-          },
-        }));
+        //initial conditions
+        if (pagination.offset === 0 && jobsData.pagination.offset === 0) {
+          setJobsData((prev) => ({
+            ...prev,
+            data: [...data?.jdList],
+            pagination: {
+              total: data.totalCount,
+              offset: pagination.offset,
+              totalPages: Math.ceil(data.totalCount / pagination.limit),
+            },
+          }));
+        } else if (
+          //condition for further offsets
+          pagination.offset > jobsData.pagination.offset
+        ) {
+          setJobsData((prev) => ({
+            ...prev,
+            data: [...prev.data, ...data?.jdList],
+            pagination: {
+              total: data.totalCount,
+              offset: pagination.offset,
+              totalPages: Math.ceil(data.totalCount / pagination.limit),
+            },
+          }));
+        }
+        setIsFetching(false);
       } catch (err) {
         console.error("api error: ", err);
       }
@@ -32,9 +59,46 @@ export const Home = () => {
     [jobsData]
   );
 
+  function getMorePosts() {
+    setTimeout(() => {
+      getJobs({ limit: 10, offset: jobsData.pagination.offset + 1 });
+    }, 2000);
+  }
+
   useEffect(() => {
     getJobs({ limit: 10, offset: 0 });
   }, []);
-  console.log("jobsData: ", jobsData);
-  return <div>Home page1111</div>;
+
+  useEffect(() => {
+    if (inView) {
+      setIsFetching(true);
+    }
+  }, [inView]);
+
+  useEffect(() => {
+    if (isFetching) getMorePosts();
+  }, [isFetching]);
+
+  return (
+    <div>
+      Job filters section
+      <div className={Styles["job-cards__container"]}>
+        {jobsData.data.length
+          ? jobsData.data.map((job) => <JobCard key={job.jdUid} job={job} />)
+          : null}
+      </div>
+      {!isFetching && jobsData.data.length ? (
+        <div ref={ref} style={{ height: "10px", width: "100%" }} />
+      ) : null}
+      {isFetching ? (
+        <p
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Loading data....
+        </p>
+      ) : null}
+    </div>
+  );
 };
